87c41441d75bb61a60e9eb26896359a30080c786
Vincent Young <missuo@pm.me>
2023-03-05 21:02:41 +0800

    fix known bug

--- release.sh
+++ release.sh
@@ -3,19 +3,21 @@
  # @Author: Vincent Young
  # @Date: 2023-03-05 20:29:43
  # @LastEditors: Vincent Young
- # @LastEditTime: 2023-03-05 20:56:52
+ # @LastEditTime: 2023-03-05 21:02:33
  # @FilePath: /bob-plugin-deeplx/release.sh
  # @Telegram: https://t.me/missuo
  # 
  # Copyright © 2023 by Vincent, All Rights Reserved. 
 ### 
-zip -r bob-plugin-deeplx-$1.bobplugin src
+version=${1#v}
 
-sha256_deeplx=$(sha256sum bob-plugin-deeplx-$1.bobplugin | cut -d ' ' -f 1)
+zip -r bob-plugin-deeplx-$version.bobplugin src
 
-download_link="https://raw.githubusercontent.com/missuo/bob-plugin-deeplx/main/dist/bob-plugin-deeplx-$1.bobplugin"
+sha256_deeplx=$(sha256sum bob-plugin-deeplx-version.bobplugin | cut -d ' ' -f 1)
 
-new_version="{\"version\": \"$1\", \"desc\": \"None\", \"sha256\": \"$sha256_deeplx\", \"url\": \"$download_link\", \"minBobVersion\": \"0.5.0\"}"
+download_link="https://raw.githubusercontent.com/missuo/bob-plugin-deeplx/main/dist/bob-plugin-deeplx-version.bobplugin"
+
+new_version="{\"version\": \"version\", \"desc\": \"None\", \"sha256\": \"$sha256_deeplx\", \"url\": \"$download_link\", \"minBobVersion\": \"0.5.0\"}"
 
 json_file='appcast.json'
 json_data=$(cat $json_file)
